08ce56c336222914f10c23fb44c9111fc2d2e6a5
Pavan_Patil_Git_Repo <102534765+pavanpatil5222@users.noreply.github.com>
2023-10-13 15:32:20 +0530

    Update Pages.java

--- BDD_Cucumber/src/test/java/Login_Pages/Pages.java
+++ BDD_Cucumber/src/test/java/Login_Pages/Pages.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+//Login Pages POM
 public class Pages {
 
     WebDriver driver;
